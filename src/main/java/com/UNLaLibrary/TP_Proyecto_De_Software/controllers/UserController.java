ec254913cfc88a5a437426a483e39f8d8df2bf99
ezequielW <ezequielwokraczka@gmail.com>
2021-05-28 23:08:10 -0300

    Vista del login

--- src/main/java/com/UNLaLibrary/TP_Proyecto_De_Software/controllers/UserController.java
+++ src/main/java/com/UNLaLibrary/TP_Proyecto_De_Software/controllers/UserController.java
@@ -1,26 +1,12 @@
 package com.UNLaLibrary.TP_Proyecto_De_Software.controllers;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.UNLaLibrary.TP_Proyecto_De_Software.converters.UserConverter;
-import com.UNLaLibrary.TP_Proyecto_De_Software.services.implementation.UserRoleService;
-import com.UNLaLibrary.TP_Proyecto_De_Software.services.implementation.UserService;
-
-
-
 
 @Controller
 public class UserController {
-
-	
-	
 	@GetMapping("/login")
 	public String login(Model model,
 					@RequestParam(name="error", required=false) String error, 
@@ -37,6 +23,6 @@ public class UserController {
 	
 	@GetMapping("/loginsuccess")
 	public String loginCheck() {
-		return "redirect:/";
+		return "redirect:/agregarDocumento";
 	}
 }
